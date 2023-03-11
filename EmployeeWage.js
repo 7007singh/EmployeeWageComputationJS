0c02b109bfe1cf03c78e2083095c774625a223bb
7007singh <shw12021982@gmail.com>
2023-03-11 17:25:07 +0530

    [Shweta]Add: check employee is present or absent]

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,13 @@
 console.log("Welcome to employee wage computation program")
+
+const IS_PRESENT = 1
+let empStatus = Math.floor(Math.random()*10)%2;  
+ 
+if(empStatus == IS_PRESENT)
+{
+    console.log("Employee is present")
+}
+else
+{
+    console.log("Employee is absent")
+}
