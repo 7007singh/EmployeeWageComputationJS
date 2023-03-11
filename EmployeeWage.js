574818dbc8398ff4a5c900ef10986ea173199105
7007singh <shw12021982@gmail.com>
2023-03-11 17:49:05 +0530

    [Shweta]Add: Calculate daily wage based on part time or full time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,13 +1,26 @@
 console.log("Welcome to employee wage computation program")
 
-const IS_PRESENT = 1
-let empStatus = Math.floor(Math.random()*10)%2;  
+//const IS_PRESENT = 1
+const FULL_TIME = 1
+const PART_TIME = 2;
+const EMP_RATE_PER_HRS = 20
+let empHrs =0, empWage = 0;
+let empStatus = Math.floor(Math.random()*10)%3;  
  
-if(empStatus == IS_PRESENT)
+switch(empStatus)
 {
-    console.log("Employee is present")
+    case FULL_TIME:
+        empHrs = 8;
+        console.log("Employee is present full time")
+        break
+        case PART_TIME:
+            empHrs = 4;
+            console.log("Employee is present half time")
+            break
+            default:
+                console.log("Employee is absent")
+                break
+
 }
-else
-{
-    console.log("Employee is absent")
-}
+empWage = empHrs*EMP_RATE_PER_HRS
+console.log("Employee wage: " + empWage)
