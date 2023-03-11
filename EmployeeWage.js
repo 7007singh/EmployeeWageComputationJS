592f034d5f9a69cf60af06829e1d2e6e56aa65b8
7007singh <shw12021982@gmail.com>
2023-03-11 18:29:10 +0530

    [Shweta]Add: Calculate wage for a month as max working days 20

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,8 +4,8 @@ console.log("Welcome to employee wage computation program")
 const FULL_TIME = 1
 const PART_TIME = 2;
 const EMP_RATE_PER_HRS = 20
-let empHrs = 0, empWage = 0;
-let empStatus = Math.floor(Math.random() * 10) % 3;
+const MAX_WORKING_DAYS = 20
+let empHrs = 0, empWage = 0, totalWage = 0;
 
 function getWorkingHrs(empStatus) {
     switch (empStatus) {
@@ -16,11 +16,14 @@ function getWorkingHrs(empStatus) {
             empHrs = 4;
             return FULL_TIME
         default:
-            console.log("Employee is absent")
             return empHrs
 
     }
 }
-empHrs = getWorkingHrs(empStatus)
-empWage = EMP_RATE_PER_HRS * empHrs
-console.log("Employee wage: " + empWage)
+for(let day = 0; day < MAX_WORKING_DAYS; day++){
+    let empStatus = Math.floor(Math.random() * 10) % 3;
+    empWage += getWorkingHrs(empStatus)
+    empWage = EMP_RATE_PER_HRS*empHrs
+    totalWage += empWage
+}
+console.log("Employee wage: " + totalWage )
