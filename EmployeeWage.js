9089a29744492f7c46cdc3ea12ae50ee744a2d5d
7007singh <shw12021982@gmail.com>
2023-03-11 18:10:34 +0530

    [Shweta]Refactor: write the function for daily working hrs

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,23 +4,23 @@ console.log("Welcome to employee wage computation program")
 const FULL_TIME = 1
 const PART_TIME = 2;
 const EMP_RATE_PER_HRS = 20
-let empHrs =0, empWage = 0;
-let empStatus = Math.floor(Math.random()*10)%3;  
- 
-switch(empStatus)
-{
-    case FULL_TIME:
-        empHrs = 8;
-        console.log("Employee is present full time")
-        break
+let empHrs = 0, empWage = 0;
+let empStatus = Math.floor(Math.random() * 10) % 3;
+
+function getWorkingHrs(empStatus) {
+    switch (empStatus) {
+        case FULL_TIME:
+            empHrs = 8;
+            return PART_TIME
         case PART_TIME:
             empHrs = 4;
-            console.log("Employee is present half time")
-            break
-            default:
-                console.log("Employee is absent")
-                break
+            return FULL_TIME
+        default:
+            console.log("Employee is absent")
+            return empHrs
 
+    }
 }
-empWage = empHrs*EMP_RATE_PER_HRS
+empHrs = getWorkingHrs(empStatus)
+empWage = EMP_RATE_PER_HRS * empHrs
 console.log("Employee wage: " + empWage)
