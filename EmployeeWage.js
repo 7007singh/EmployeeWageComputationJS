6b99d25312005f73620643f264a6093ed6155333
7007singh <shw12021982@gmail.com>
2023-03-13 16:34:17 +0530

    [Shweta]Add: Calculate Wages till a condition of total working hours of 160 or max days of 20 is reached for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,25 +5,27 @@ const FULL_TIME = 1
 const PART_TIME = 2;
 const EMP_RATE_PER_HRS = 20
 const MAX_WORKING_DAYS = 20
-let empHrs = 0, empWage = 0, totalWage = 0;
+const MAX_WORKING_HRS = 160
+let empHrs = 0, empWage = 0, totalWage = 0, workingDays = 0;
 
 function getWorkingHrs(empStatus) {
     switch (empStatus) {
         case FULL_TIME:
             empHrs = 8;
-            return PART_TIME
-        case PART_TIME:
-            empHrs = 4;
-            return FULL_TIME
-        default:
             return empHrs
-
+        case PART_TIME:
+            empHrs = 4; 
+            return empHrs
+        default:
+            return 0
     }
 }
-for(let day = 0; day < MAX_WORKING_DAYS; day++){
-    let empStatus = Math.floor(Math.random() * 10) % 3;
-    empWage += getWorkingHrs(empStatus)
-    empWage = EMP_RATE_PER_HRS*empHrs
-    totalWage += empWage
+
+while(empHrs <= MAX_WORKING_HRS && workingDays < MAX_WORKING_DAYS){
+    workingDays++
+    let empStatus = Math.floor(Math.random() * 10) % 3
+    empHrs += getWorkingHrs(empStatus)
 }
-console.log("Employee wage: " + totalWage )
+empWage = EMP_RATE_PER_HRS*empHrs
+totalWage += empWage
+console.log("Employee wage: " + totalWage + " total hours: " + empHrs + " total days: " + workingDays)
